e8f8c8fb1e9ab90474f580912791f21b8af60c2f
Riley-H-99 <rhardy216@gmail.com>
2022-02-20 03:51:45 -0700

    Added Temperature cards

---
+++ src/Components/Weather.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import WeatherCard from "./WeatherCard";
+
+const Weather = () => {
+    const [weather, setWeather] = useState([]);
+
+    useEffect(() => {
+        (async() => {
+            const lat = 36.356766;
+            const long = -94.236638;
+            const res = await axios.get(`https://api.weather.gov/points/${lat},${long}`);
+            //const resJson = await res.json();
+            const response = await axios.get(res.data.properties.forecast);
+            //const finalJson = await response.json();
+            setWeather(response.data.properties.periods);
+        }) ();
+    }, []);
+
+    return (
+        weather.map((card) => <WeatherCard temp= {card.temperature}/>)
+    )
+}
+
+export default Weather;
---
+++ src/Components/WeatherCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WeatherCard = props => {
+    return (
+        <div>
+            <h1> {props.temp} </h1>
+        </div>
+    )
+}
+
+export default WeatherCard;
