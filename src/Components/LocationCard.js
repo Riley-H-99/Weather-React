2d1e846f391e4e8f679768203cdf5a8635e8d7e9
Riley-H-99 <rhardy216@gmail.com>
2022-03-25 01:29:14 -0600

    Reworked Weather Data Handling
    change app logic to pull weather data based on local storage on initial load or parameters passed in and generate weather card array in index.js

--- src/Components/LocationCard.js
+++ src/Components/LocationCard.js
@@ -4,9 +4,14 @@ import './Form.css';
 
 
 const LocationCard = () => {
+    function newData() {
+        localStorage.setItem("lattitude", document.getElementById("lattitude").value);
+        localStorage.setItem("longitude", document.getElementById("longitude").value);
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={newData}>
                 <label class="default" for="lattitude">Lattitude:</label>
                 <input type="number" id="lattitude" name="lattitude"></input>
                 <label class="right" for="longitude">Longitude:</label>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
-import Weather from './Components/Weather';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LocationCard from'./Components/LocationCard.js';
+import WeatherCard from './Components/WeatherCard';
+import axios from 'axios';
 
 
 const App = () => {
+  const [day, setDay] = useState([]);
+  const [night, setNight] = useState([]);
+
+  useEffect(() => {
+    (async() => {
+        let params = new URLSearchParams(window.location.search);
+        //console.log([...params]);
+        let lat, long;
+        if (!params.length) {
+          lat = localStorage.getItem("lattitude");
+          long = localStorage.getItem("longitude");
+        } else {
+          lat = params.get('lattitude');
+          long = params.get('longitude');
+        }
+        const res = await axios.get(`https://api.weather.gov/points/${lat},${long}`);
+        const response = await axios.get(res.data.properties.forecast);
+        const periods = response.data.properties.periods;
+        let dayObjects = [];
+        let nightObjects = [];
+        periods.forEach(period => {
+          if (period.isDaytime) {
+            dayObjects.push(period);
+          } else {
+            nightObjects.push(period);
+          }
+        })
+        if (!periods[0].isDaytime) {
+          dayObjects.pop();
+          nightObjects.shift();
+        }
+        setDay(dayObjects);
+        setNight(nightObjects);
+    }) ();
+  }, []);
+
   return (
     <div>
       <header class="App-Header">
@@ -14,9 +51,14 @@ const App = () => {
       </header>
       <body>
         <LocationCard />
-        <div>
-          <Weather />
-        </div>
+        <table class="WeatherArray" id="container">
+          <div class="WeatherArray">
+            {day.map((card) => <WeatherCard temp= {card.temperature} name= {card.name}/>)}
+          </div>
+          <div class="WeatherArray">
+            {night.map((card) => <WeatherCard temp= {card.temperature} name= {card.name}/>)}
+          </div>
+        </table>
       </body>
     </div>
   );
